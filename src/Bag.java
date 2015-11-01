47133b8bce0d5f1d1e83a23a1aef146cb235f56f
Manuel Perez <mperez@bassan.co>
2015-11-01 11:58:41 -0500

    Base for Week 8 - Assignmet - Project 2

---
+++ src/Bag.java
@@ -0,0 +1,136 @@
+public class Bag implements Cloneable{
+	
+	private Object[] item;
+	int size,count;
+	
+	public Bag(int size){
+		this.size = size;
+		this.count = 0;
+		item = new Object[size];
+	}
+
+	public boolean isEmpty(){
+		return count == 0;
+	}
+
+	/*public Object getlast(){
+	 Object result = item[count-1];
+	 count--;
+	 return result;
+ }
+	 */
+
+	public boolean isFull(){
+		return count == size;
+	}
+
+	public boolean add(Object o){
+		if(isFull()) {
+			System.out.println("Bag is full");
+			return false;
+		}
+		item[count++] = o;
+		return true;
+	}
+
+	public Object grab(){
+		if(isEmpty()){
+			System.out.println("No item");
+			return false;
+		}
+		int i = (int)(Math.random()*count);
+		return item[i];
+	}
+
+	public int size(){
+		return size;
+	}
+
+	public int count(){
+		return count;
+	}
+
+	public boolean contains(Object o){
+		int jg = 0;
+		for (int i = 0; i < count; i++) {
+			if (item[i].equals(o)) jg = 1;
+		}
+		if(jg == 1)return true;
+		else return false;
+	}
+
+	public boolean remove(Object n){
+		for (int i = 0; i < count; i++) {
+			if (item[i] == n) {
+				item[i] = null;
+				while(i<count-1) {
+					item[i] = item[i+1];
+					item[i+1] = null;
+					i++;
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	@Override 
+	public boolean equals(Object other){
+		int jg = 0;
+
+		if(other == this)
+			return true; 
+		if(other == null)
+			return false;
+		if(!(other instanceof Bag))
+			return false;
+
+		final Bag s = (Bag)other;
+
+		if((this.size()!= s.size())||(this.count()!=s.count())){
+			return false;
+		}else
+		{
+			while(!this.isEmpty()&&!s.isEmpty()){
+				for(int i=0;i<this.count;i++){
+					for(int j=0;j<this.count;j++){
+						if(this.item[i].equals(s.item[j]))jg++;
+					}
+				}
+				if(jg == this.count)return true;
+				/*if(this.getlast().equals(s.getlast()) == false)
+			 {
+				 System.out.println("You added:");
+				 return false;
+			 }*/
+			}
+			return false;
+		}
+	}
+	
+	@Override 
+	public int hashCode(){
+		int sum = 0;
+		int result = 17;
+		result = 31*result + size;
+		result = 31*result + count;
+		for(int i=0;i<=count-1;i++){
+			sum = sum + item[i].hashCode();
+		}
+		result = 31*result + sum;
+		return result;
+	}
+	
+	@Override 
+	public Bag clone(){
+		try {
+			Bag result = (Bag) super.clone();
+			result.item = item.clone();
+			return result;
+		}catch (CloneNotSupportedException e){
+			throw new AssertionError();
+		}
+	}
+	
+}
+
---
+++ src/Stack.java
@@ -0,0 +1,100 @@
+public class Stack implements Cloneable{
+
+	private Object[] data;
+	private int top = -1;
+	int maxsize;
+
+	public Stack(int maxsize) {
+		this.maxsize = maxsize;
+		data = new Object[maxsize];
+	}
+
+	public int getSize(){
+		return maxsize;
+	}
+
+	public int getCount(){
+		return top+1;
+	}
+
+	public Object getTop(){
+		return data[top];
+	}
+
+	public boolean isEmpty(){
+		return top == -1;
+	}
+
+	public boolean isFull(){
+		return top + 1 == maxsize;
+	}
+
+	public void push(Object o){
+		if(isFull()){
+			System.out.println("Fail to push,The Stack is full");
+		}
+		data[++top] = o;
+		System.out.println("New object has been pushed");
+	}
+
+	public Object pop(){
+
+		if(isEmpty()){
+			System.out.println("No item");
+		}
+		Object t = data[top];
+		data[top--]=null;
+		return t;
+	}
+	
+	@Override 
+	public boolean equals(Object other){
+
+		if(other == this)
+			return true; 
+		if(other == null)
+			return false;
+		if(!(other instanceof Stack))
+			return false;
+
+		Stack s = (Stack)other;
+		//
+
+		if((this.getSize()!= s.getSize())||(this.getCount()!=s.getCount())){
+			return false;
+		}
+		//
+		else
+		{
+			while(!this.isEmpty()&&!s.isEmpty()){
+				if(this.pop().equals(s.pop()) == false)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+	
+	@Override 
+	public int hashCode(){
+		int result = 17;
+		result = 31*result + maxsize;
+		result = 31*result + top;
+		for(int i=0;i<=top;i++){
+			result = 31*result + data[i].hashCode();
+		}
+		return result;
+	}
+	
+	@Override 
+	public Stack clone(){
+		try {
+			Stack result = (Stack) super.clone();
+			result.data = data.clone();
+			return result;
+		}catch (CloneNotSupportedException e){
+			throw new AssertionError();
+		}
+	}
+}
